585c0557e3d9a7f2adeae89c94be075c2dce6246
saipavankumar0702 <saipavan.0702@gmail.com>
2025-08-30 14:26:37 +0530

    Inital commit

---
+++ terraform.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "vprofileactions0702"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_version = "~> 1.13.0"
+}
+
+# first edit
+# modified URL in last step of terraform.yml
+# modifeid the URL before script
+# checking only stage and main
