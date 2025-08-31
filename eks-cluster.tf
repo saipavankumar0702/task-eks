00afb6e04a11db849f629e5b285bd5e87c0c3290
saipavankumar0702 <saipavan.0702@gmail.com>
2025-08-31 22:03:03 +0530

    v7

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -8,12 +8,16 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # Enable secrets encryption using your custom KMS key
   cluster_encryption_config = {
     resources        = ["secrets"]
     provider_key_arn = aws_kms_key.jenkins_key.arn
   }
 
   create_kms_key = false
+
+  # Explicitly set IAM policy prefix to avoid dynamic evaluation
+  iam_role_policy_prefix = "arn:aws:iam::aws:policy"
 
   eks_managed_node_groups = {
     one = {
